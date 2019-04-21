a28e4ba80179bcf151a0d1f4d2961850a88e08f4
Andrew Wojciechowski <wojciechowskia@msoe.edu>
2019-04-21 16:15:50 -0500

    Converted views to use erb instead of html

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,11 +4,16 @@ class HomeController < ApplicationController
   def index
   end
 
-  # GET /preview
+  # POST home/preview
   def preview
     kramdown_html = Kramdown::Document.new(params[:text]).to_html
     render json: {
         html: kramdown_html
     }
+  end
+
+  #POST home/submit
+  def submit
+
   end
 end
