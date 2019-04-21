90eeb49e1f7b55daef6e31403d87c3601503fdbd
Andrew Wojciechowski <wojciechowskia@msoe.edu>
2019-04-21 11:11:57 -0500

    Stubbed out a service class for submiting a new jekyll post

---
+++ app/services/github_service.rb
@@ -0,0 +1,8 @@
+class GithubService
+  def submit_post(post_markdown, author)
+    #TODO: Authentication
+    #TODO: Create Branch for new post
+    #TODO: Commit and push new post
+    #TODO: Create pull request for new post
+  end
+end
