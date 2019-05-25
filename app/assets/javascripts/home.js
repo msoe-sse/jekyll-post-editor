363a5ef69b8b91d7a8ba289cf88e1c8e51f5b123
andy-wojciechowski <wojciechowskia@msoe.edu>
2019-05-24 20:19:11 -0500

    Removed CoffeeScript

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,15 @@
+$(function() {
+  $('#get-preview-button').click(function() {
+    fetch('home/preview', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({'text': $('#markdownArea').val()})
+    })
+    .then(resp => resp.json())
+    .then(function(data) {
+      $('#previewArea').html(data.html);
+    })
+  });
+});
