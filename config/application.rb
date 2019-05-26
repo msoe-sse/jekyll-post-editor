1fe70b2182053ae329f042d068e6faa7a1a8bd5f
andy-wojciechowski <wojciechowskia@msoe.edu>
2019-05-26 13:58:40 -0500

    Adding some more require statements in the application class

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,13 @@
 require_relative 'boot'
 
-require 'action_controller/railtie'
-require 'active_support/all'
+require 'rails'
+require 'active_model/railtie' 
+require 'active_job/railtie'
+require 'action_controller/railtie' 
+require 'action_mailer/railtie' 
+require 'action_view/railtie' 
+require 'sprockets/railtie' 
+require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
