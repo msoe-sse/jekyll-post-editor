92ece918168d5686b6afc2b623661b0e00f5c32f
andy-wojciechowski <wojciechowskia@msoe.edu>
2019-05-26 14:07:12 -0500

    Removed setting the fixtures in the ActiveSupport TestCase class

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,4 +1,4 @@
 require 'test_helper'
 
-class HomeControllerTest < ActiceSupport::TestCase
+class HomeControllerTest < ActiveSupport::TestCase
 end
--- test/services/github_service_test.rb
+++ test/services/github_service_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class GithubServiceTest < ActiceSupport::TestCase
+class GithubServiceTest < ActiveSupport::TestCase
   test 'authenticate should return nil on failed authentication' do 
     result = GithubService.authenticate('test', 'test')
     #TODO: Finish this test
