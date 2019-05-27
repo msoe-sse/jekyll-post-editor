43729bf97509ae44a76a18049a8a160b4edb80ee
andy-wojciechowski <wojciechowskia@msoe.edu>
2019-05-26 19:22:53 -0500

    Properly implemented validation messages for the login process

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,11 +3,11 @@ class HomeController < ApplicationController
   def login
     client = GithubService.authenticate(params[:username], params[:login][:password])
     if client == :unauthorized
-      #@validation_message = 'Invalid GitHub username or password'
-      redirect_to '/'
+      validation_message = 'Invalid GitHub username or password'
+      redirect_to '/', :alert => validation_message
     elsif client == :not_in_organization
-      #@validation_message = 'The GitHub user provided is not apart of the msoe-sse GitHub orginization. Please contact the SSE Webmaster for assistance.'
-      redirect_to '/'
+      validation_message = 'The GitHub user provided is not apart of the msoe-sse GitHub orginization. Please contact the SSE Webmaster for assistance.'
+      redirect_to '/', :alert => validation_message
     else
       session[:github_client] = client
       redirect_to '/post/list'
