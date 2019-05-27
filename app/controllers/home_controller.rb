a337285d1fcb2e4a28b53a9b5e6396110b060ec1
andy-wojciechowski <wojciechowskia@msoe.edu>
2019-05-26 19:06:41 -0500

    Started hooking up the login controller method

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,16 @@
 class HomeController < ApplicationController
   #POST home/login
   def login
-    #TODO: Implement this
+    client = GithubService.authenticate(params[:username], params[:login][:password])
+    if client == :unauthorized
+      #@validation_message = 'Invalid GitHub username or password'
+      redirect_to '/'
+    elsif client == :not_in_organization
+      #@validation_message = 'The GitHub user provided is not apart of the msoe-sse GitHub orginization. Please contact the SSE Webmaster for assistance.'
+      redirect_to '/'
+    else
+      session[:github_client] = client
+      redirect_to '/post/list'
+    end
   end
 end
