92e662dc8aaa7847249dab0e74c5e9126a394358
andy-wojciechowski <wojciechowskia@msoe.edu>
2019-06-09 09:14:37 -0500

    Linked a GitHub authentication article in the GitHub controller

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -2,7 +2,9 @@
 # This controller contains specific code for handling GitHub specific urls
 class GithubController < ApplicationController
   ##
-  # This method handles the oauth callback url after a user authorizes the post editor with their GitHub account
+  # This method handles the oauth callback url after a user authorizes 
+  # the post editor with their GitHub account. For more information about this controller method
+  # see https://developer.github.com/v3/guides/basics-of-authentication/
   def callback
     session_code = params[:code]
     session[:access_token] = GithubService.get_oauth_access_token(session_code)
