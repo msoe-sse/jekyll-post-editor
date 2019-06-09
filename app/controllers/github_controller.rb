868940e07b246e02e1fdd09115f2ef887567ee35
andy-wojciechowski <wojciechowskia@msoe.edu>
2019-06-09 09:13:01 -0500

    Added tests for the GitHub controller

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -1,6 +1,11 @@
+##
+# This controller contains specific code for handling GitHub specific urls
 class GithubController < ApplicationController
+  ##
+  # This method handles the oauth callback url after a user authorizes the post editor with their GitHub account
   def callback
     session_code = params[:code]
     session[:access_token] = GithubService.get_oauth_access_token(session_code)
+    redirect_to '/'
   end
 end
---
+++ test/controllers/github_controller_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+require 'mocha/setup'
+
+class GithubControllerTest < ActionDispatch::IntegrationTest
+  test 'the callback url should rredirect back to the index after getting the oauth access token' do
+    #Arrange
+    GithubService.expects(:get_oauth_access_token).with('session code').returns('access token')
+
+    #Act
+    get '/github/callback', params: { code: 'session code' }
+
+    #Assert
+    assert_equal 'access token', session[:access_token]
+    assert_redirected_to '/'
+  end
+end
