adfae440bc8ee97287c6d113d3eb9a0f21bc962c
andy-wojciechowski <wojciechowskia@msoe.edu>
2019-06-09 19:13:22 -0500

    Adding initial auth code to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,28 @@ require_relative '../error/error'
 class ApplicationController < ActionController::Base
   include Error::ErrorHandler
   protect_from_forgery prepend: true
+  before_action :check_authentication
+
+  private
+  def check_authentication
+    if !authenticated?
+      authenticate!
+    else
+      access_token = session[:access_token]
+      is_token_valid = GithubService.check_access_token(access_token)
+      if !is_token_valid
+        session[:access_token] = nil
+        authenticate!
+      end
+    end
+  end
+
+  def authenticated?
+    session[:access_token]
+  end
+
+  def authenticate!
+    auth_url = GithubService.get_authorization_url
+    redirect_to auth_url
+  end
 end
