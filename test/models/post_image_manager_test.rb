ae3fd10f73821576986f137b8342e6917be3ab40
andy-wojciechowski <wojciechowskia@msoe.edu>
2019-08-18 16:15:46 -0500

    Fixed unit tests

--- test/models/post_image_manager_test.rb
+++ test/models/post_image_manager_test.rb
@@ -4,6 +4,7 @@ require 'mocha/setup'
 class PostImageManagerTest < ActiveSupport::TestCase
   test 'add_file should create a new PostImageUploader and cache the file' do 
     # Arrange
+    setup_clear_mocks
     PostImageManager.instance.clear
     mock_file = create_mock_action_dispatch_file('my file.jpg')
     PostImageUploader.any_instance.expects(:cache!).with(mock_file).once
@@ -18,6 +19,7 @@ class PostImageManagerTest < ActiveSupport::TestCase
 
   test 'add_file should remove any previous uploaders that have the same filename as the file being added' do 
     # Arrange
+    setup_clear_mocks
     PostImageManager.instance.clear
     mock_file = create_mock_action_dispatch_file('my file.jpg')
     PostImageUploader.any_instance.expects(:cache!).with(mock_file).twice
@@ -34,8 +36,10 @@ class PostImageManagerTest < ActiveSupport::TestCase
 
   test 'clear should clear all PostImageUploader instances from the manager' do 
     # Arrange
+    setup_clear_mocks
     PostImageManager.instance.clear
     mock_file = create_mock_action_dispatch_file('my file.jpg')
+
     PostImageUploader.any_instance.expects(:cache!).with(mock_file).once
 
     # Act
@@ -45,4 +49,14 @@ class PostImageManagerTest < ActiveSupport::TestCase
     # Assert
     assert_equal 0, PostImageManager.instance.uploaders.length
   end
+
+  private
+    def setup_clear_mocks
+      mock_uploader = create_mock_uploader('preview_my file.jpg', 'my cache/preview_my file.jpg', nil)
+      preview_uploader = create_preview_uploader('my file', mock_uploader)
+
+      PostImageUploader.any_instance.expects(:preview).returns(preview_uploader).at_least(0)
+      PostImageUploader.any_instance.expects(:remove!).at_least(0)
+      Dir.expects(:delete).at_least(0)
+    end
 end
